8ef70bfc75e6aafef1c69f2e6fef97675620c119
Takuya Yamasaki <takuyaya@yahoo-corp.jp>
2019-12-31 18:07:16 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #! /usr/bin/env bash
 
-cd
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
+ln -s -f .tmux/.tmux.conf ${HOME}
+cp ${HOME}/.tmux/.tmux.conf.local ${HOME}
